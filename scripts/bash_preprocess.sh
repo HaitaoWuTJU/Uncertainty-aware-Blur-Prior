f8a67b4995fc9a6532f26f515489e086b97be590
Hai-Tao Wu <61724827+HaitaoWuTJU@users.noreply.github.com>
2026-02-28 19:42:01 +0800

    update

--- scripts/bash_preprocess.sh
+++ scripts/bash_preprocess.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /xxxx/Uncertainty-aware-Blur-Prior
 
+# python preprocess/process_resize.py --type eeg
+# python preprocess/process_resize.py --type meg
+
 ## EEG
 # for subject in {1..10}
 # do
@@ -13,6 +16,3 @@ do
   python preprocess/process_meg.py --subject $subject
 done
 
-
-# python preprocess/process_resize.py --type eeg
-# python preprocess/process_resize.py --type meg
